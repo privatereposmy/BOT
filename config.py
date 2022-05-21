d7d3c4e0b91243ed6f4022c6db93bb14135986cb
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-05-21 21:33:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
 
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-      API_ID = int(os.environ.get("API_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      OWNER_ID = int(os.environ.get("OWNER_ID"))
+      BOT_TOKEN = os.environ.get("5302212624:AAFgg9qi_J4iKXxrFGfjx_RM3uq5Kb-s1k0", "")
+      API_ID = int(os.environ.get("8406611",))
+      API_HASH = os.environ.get("5820bc246505e0ff60af5391d649f9a6")
+      OWNER_ID = int(os.environ.get("1467649219"))
 
