516c6620e2e9bd31ff5236064287e60aef5799e5
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-05-21 22:38:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
 
       BOT_TOKEN = os.environ.get("5302212624:AAFgg9qi_J4iKXxrFGfjx_RM3uq5Kb-s1k0", "")
-      API_ID = int(os.environ.get("8406611",))
-      API_HASH = os.environ.get("5820bc246505e0ff60af5391d649f9a6")
-      OWNER_ID = int(os.environ.get("1467649219"))
+      API_ID = int(os.environ.get("840611",))
+      API_HASH = os.environ.get("5820bc24605e0ff60af5391d649f9a6")
+      OWNER_ID = int(os.environ.get("167649219"))
 
