89efb22ccddf4bd1bece3057400d12ccd5f0b73d
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-07-28 17:56:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,13 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from variables import STAT_STICK, PICS, DELAY
+from variables import DELAY, PICS
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start_message(client, msg):
     await message.reply_chat_action("Typing")
-    m=await message.reply_sticker(STAT_STICK)
     await asyncio.sleep(DELAY)
-    await m.delete()
     await message.reply_photo(
        photo=random.choice(PICS),
        caption=f"**Sorry bro,You didn't Joined Our Updates Channel Join now and start again🙏**",
