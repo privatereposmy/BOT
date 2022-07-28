45429e54d91c5a3e1756e8aef805ad24167646aa
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-07-28 11:23:32 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/privatereposmy/Text-Bot.git /Stylish-Text-Bot
-cd /Stylish-Text-Bot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
