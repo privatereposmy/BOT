82479e6c146770dbdb6ed969769379884fb85dab
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-07-28 17:34:14 +0530

    Update and rename bot.py to main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+From pyrogram import Client
+
+
+BOT = Client(
+    "BOT",
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611",
+    bot_token="5545165624:AAGUHPgUtwrBTh7IKnVMcFzI0Rk-KY4I1cw",
+    plugins=dict(root="Plugins")
+)
+
+
+BOT.run
