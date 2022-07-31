122c1f4910e7b294a9f5b6b2eb83bfc59361d394
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-07-31 22:28:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ BOT = Client(
     "BOT",
     api_hash="5820bc246505e0ff60af5391d649f9a6",
     api_id="8406611",
-    bot_token="5545165624:AAGUHPgUtwrBTh7IKnVMcFzI0Rk-KY4I1cw",
+    bot_token="5545165624:AAGWkOcageIuHa7Ola0LDA-kBOXWElSrbNw",
     plugins=dict(root="plugins")
 )
 
