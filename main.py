fc4d2f95e709fdd531000f8d030cb2f3df9816d4
privatereposmy <105050877+privatereposmy@users.noreply.github.com>
2022-08-02 17:02:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from pyrogram import Client
+from bot import Bot
 
-
-BOT = Client(
-    "BOT",
-    api_hash="5820bc246505e0ff60af5391d649f9a6",
-    api_id="8406611",
-    bot_token="5545165624:AAGWkOcageIuHa7Ola0LDA-kBOXWElSrbNw",
-    plugins=dict(root="plugins")
-)
-
-
-BOT.run
+Bot().run()
